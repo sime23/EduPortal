fb27937f3050c79f8fcf7ce8d733ede859f27704
sime23 <simebryan2003@gmail.com>
2026-03-30 02:44:16 +0100

    update assignments fixed && db.php

--- includes/db.php
+++ includes/db.php
@@ -5,7 +5,7 @@
 define('DB_HOST', 'localhost');
 define('DB_NAME', 'ass');
 define('DB_USER', 'eduportal_admin');
-define('DB_PASS', 'eduportal_admin237');
+define('DB_PASS', 'eduportal237');
 define('DB_CHARSET', 'utf8mb4');
 
 $dsn = sprintf('mysql:host=%s;dbname=%s;charset=%s', DB_HOST, DB_NAME, DB_CHARSET);
